364164253cb0f74f7bacb5421d0ddf076311593d
Sneyder M <sneider.caicedo@gmail.com>
2024-04-22 22:39:23 -0500

    feat: Obtención de imagenes del blob de storage

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const express = require('express')
-const app = express()
+const { BlobServiceClient } = require('@azure/storage-blob');
+const express = require('express');
+const fetch = require('node-fetch');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+require('dotenv').config({ path: './env/.env' });
 
-app.listen(3000)
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+
+app.get('/api/images/:imageName', async (req, res) => {
+  const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
+  const blobName = req.params.imageName;
+
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  
+  try {
+    const blobClient = containerClient.getBlobClient(blobName);
+    
+    const response = await blobClient.download();
+    const stream = response.readableStreamBody;
+
+    res.setHeader('Content-Type', response.contentType);
+    stream.pipe(res);
+
+    console.log('Imagen obtenida correctamente');
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Hubo un error al obtener la imagen');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
